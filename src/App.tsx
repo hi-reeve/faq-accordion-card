7fecfa121c6ea1998a0cd14af02f4faae66e0671
Christopher Reeve <reeveworkmail@gmail.com>
2021-05-21 22:42:02 +0700

    :sparkles: update

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,33 @@ import style from "@/App.module.scss";
 import useMediaQuery from "./hooks/useMediaQuery";
 const App: React.FC<{}> = () => {
     const isLarge = useMediaQuery(525);
-    const datas: { title: string; content: string }[] = [
+    const datas: { id: number; title: string; content: string }[] = [
         {
+            id: 1,
             title: "How many team members can I invite?",
             content:
                 "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
         },
         {
+            id: 2,
             title: "What is the maximum file upload size?",
             content:
                 "No more than 2GB. All files in your account must fit your allotted storage space.",
         },
         {
+            id: 3,
             title: "How do I reset my password?",
             content:
                 "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
         },
         {
+            id: 4,
             title: "Can I cancel my subscription?",
             content:
                 "Yes! Send us a message and we’ll process your request no questions asked.",
         },
         {
+            id: 5,
             title: "Do you provide additional support?",
             content:
                 "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
@@ -53,12 +58,21 @@ const App: React.FC<{}> = () => {
                             />
                         </Fragment>
                     )}
+                    {!isLarge && (
+                        <Fragment>
+                            <img
+                                src={BoxDesktop}
+                                className={style["faq--box"]}
+                            />
+                        </Fragment>
+                    )}
                 </div>
                 <div className={style["faq--content"]}>
                     <h1 className={style["faq--title"]}>FAQ</h1>
                     {datas.map(data => {
                         return (
                             <Accordion
+                                key={data.id}
                                 title={data.title}
                                 content={data.content}
                             ></Accordion>
